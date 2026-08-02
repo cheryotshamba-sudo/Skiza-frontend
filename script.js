a516e771719ab553adab5b96985f2357d94519bd
cheryotshamba-sudo <cheryotshamba@gmail.com>
2026-08-02 23:49:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function startNow() {
-    window.location.href = "next.html";
-}
-
-function showMessage() {
-    alert("Welcome! Continue to the next step.");
+    window.location.href = "wheel.html";
 }
