8b16ef735e16028719924a7b18c40bce686a8b22
cheryotshamba-sudo <cheryotshamba@gmail.com>
2026-08-03 21:45:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const video = document.getElementById("promoVideo");
+
+document.addEventListener("click", function () {
+    video.muted = false;
+    video.volume = 1;
+    video.play();
+}, { once: true });
+
 function startNow() {
-    window.location.href = "wheel.html";
+    window.location.href = "register.html";
 }
